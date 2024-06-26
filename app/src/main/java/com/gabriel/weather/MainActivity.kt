73a9e6047debb68ee34d30b140c6b4eeca818e03
Gabriel Ferrari <ghcferrari75@gmail.com>
2024-06-25 23:23:53 -0300

    Error Solved

--- app/src/main/java/com/gabriel/weather/MainActivity.kt
+++ app/src/main/java/com/gabriel/weather/MainActivity.kt
@@ -128,12 +128,12 @@ class MainActivity : AppCompatActivity() {
                 binding.lottieAnimationView.setAnimation(R.raw.cloud)
             }
 
-            "Light Rain", "Drizzle", "Moderate Rain", "Showers", "Heavy Rain" -> {
+            "Rain", "Light Rain", "Drizzle", "Moderate Rain", "Showers", "Heavy Rain" -> {
                 binding.root.setBackgroundResource(R.drawable.rain_background)
                 binding.lottieAnimationView.setAnimation(R.raw.rain)
             }
 
-            "Light Snow", "Moderate Snow", "Heavy Snow", "Blizzard" -> {
+            "Snow", "Light Snow", "Moderate Snow", "Heavy Snow", "Blizzard" -> {
                 binding.root.setBackgroundResource(R.drawable.snow_background)
                 binding.lottieAnimationView.setAnimation(R.raw.snow)
             }
